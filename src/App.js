cf01594a55134326daeb1bc5d149385ffd60083a
Jasdeepkaur1381 <kjasdeep219@gmail.com>
2023-06-21 03:34:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { questions } from './api.js';
+import MyAccordion from './MyAccordian';
+const App = () => {
+  const [data, setdata] = useState(questions);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {
+        data.map((curElem) => {
+          const { id, question, answer } = curElem;
+          return <MyAccordion key={id}
+            question={question}
+            answer={answer}
+
+          />
+        })
+      }
+    </>
+  )
 }
 
 export default App;
